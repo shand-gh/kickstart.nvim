1c863bfbae7196b576343eeb885185f68ad3d922
shand-gh <shand-gh@github.com>
2024-10-16 19:23:49 +0000

    Changes black global line length to 120

--- init.lua
+++ init.lua
@@ -631,6 +631,11 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
+      formatters = {
+        black = {
+          prepend_args = { '--line-length', '120' },
+        },
+      },
     },
   },
 
